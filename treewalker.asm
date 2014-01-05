3ae813ef390cc6b722b5e4f6f0de88730cc89e9d
willdavis <willdavis84@gmail.com>
2014-01-04 21:33:17 -0800

    added calloc and free calls to treewalker callback function

--- treewalker.asm
+++ treewalker.asm
@@ -1,6 +1,6 @@
 ; Description: call ftw() on a file path and return the directory structure
 ; Author: Bill Davis
-; Prototype: void TreeWalker(const char* file_path)
+; Prototype: struct data file_data[]* TreeWalker(const char* file_path)
 
 ; int ftw(
 ;	const char *dirpath,
@@ -8,21 +8,33 @@
 ;	int nopenfd);
 ; )
 
-%define	MAX_OPEN_FILES 64	;more files = more memory, but faster ftw() speeds
+%define	MAX_OPEN_FILES 64			;more files = more memory, but faster ftw() speeds
+%define SIZEOF_FILE_DATA_ELEMENT 12	;sizeof struct data{} (12 bytes)
 
 [SECTION .bss]
-source_file_path: resd 1	;const char* file_path 
+source_file_path: 		resd 1	;const char* file_path
+file_data_array_ptr:	resd 1	;struct data file_data[]*
+file_data_array_size:	resd 1	;int file_data_array_size
+
+; struct data {
+;	const char *fpath
+;	const struct stat *sb
+;	int typeflag
+; }
 
 [SECTION .data]
 debug_show_current_path: db "Checking directory structure: '%s'",10,0
 print_file_found:	db "Found: '%s' (typeflag: %i)",10,0
-printFTWComplete:	db "SUCCESS: directory structure scanned without errors!",10,0
+printFTWComplete:	db "SUCCESS: directory structure scanned without errors! (files found: %i)",10,0
 printFTWError:	db "ERROR: file tree walker encountered a problem...",10,0
 
 	
 section .txt
 	extern ftw
 	extern printf
+	
+	extern calloc
+	extern free
 
 	global TreeWalker
 
@@ -33,6 +45,8 @@ TreeWalker:
 	mov eax, [ebp+8]					;store const char* file_path in eax
 	mov dword [source_file_path], eax	;save const char* file_path for later
 	xor eax,eax							;clear eax
+
+	mov dword [file_data_array_size], 0	;initialize file_data_array_size to zero
 
 	; let the user know we're starting to walk the directory
 	push dword [source_file_path]
@@ -58,9 +72,10 @@ TreeWalker:
 	je .error
 
 	; yay! no errors.  let the user know and then exit.
+	push dword [file_data_array_size]
 	push printFTWComplete
 	call printf
-	add esp,4
+	add esp,8
 	jmp .exit
 
 .error:
@@ -87,6 +102,27 @@ callback:
 	push print_file_found
 	call printf
 	add esp,12
+	
+	; increment size and allocate new file_data[] with calloc
+	inc dword [file_data_array_size]
+	
+	; void *calloc(size_t nmemb, size_t size);
+	push dword SIZEOF_FILE_DATA_ELEMENT
+	push dword [file_data_array_size]
+	call calloc
+	add esp,8
+	mov dword [file_data_array_ptr],eax
+	
+	;temporary debug! free the memory!
+	push dword [file_data_array_ptr]
+	call free
+	add esp,4
+	mov dword [file_data_array_ptr], 0
+
+	; get previous file_data[] if available
+	; copy previous file_data[] elements to new file_data[]
+	; add new data from this callback
+	; free previous file_data[]'s memory
 
 	mov eax,0
 	leave
