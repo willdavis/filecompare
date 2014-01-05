5faf80114cef240564c7338be6870c39b1a85fb9
willdavis <willdavis84@gmail.com>
2014-01-04 20:17:02 -0800

    set max_opened_files variable to 20 and updated comments

--- treewalker.asm
+++ treewalker.asm
@@ -9,9 +9,11 @@
 ; )
 
 [SECTION .bss]
-file_path: resd 1	;const char* file_path 
+source_file_path: resd 1	;const char* file_path 
 
 [SECTION .data]
+max_open_files: dd 20		; set maximum number of open files.
+
 debug_show_current_path: db "Checking directory structure: '%s'",10,0
 print_file_found:	db "Found: '%s' (typeflag: %i)",10,0
 printFTWComplete:	db "SUCCESS: directory structure scanned without errors!",10,0
@@ -28,33 +30,41 @@ TreeWalker:
 	push ebp
 	mov ebp,esp		;setup stack frame for function
 	
-	mov eax, [ebp+8]
-	mov dword [file_path], eax	;save file path in global variable
+	mov eax, [ebp+8]					;store const char* file_path in eax
+	mov dword [source_file_path], eax	;save const char* file_path for later
+	xor eax,eax							;clear eax
 
-	push dword [file_path]
+	; let the user know we're starting to walk the directory
+	push dword [source_file_path]
 	push debug_show_current_path
 	call printf
 	add esp,8
 
-	xor eax,eax		;clear eax
-
 	;; call ftw()
-	mov ebx, 10
-	push ebx
+	; int ftw(
+	;	const char *dirpath,
+	;	int (*fn) (const char *fpath, const struct stat *sb, int typeflag),
+	;	int nopenfd);
+	; )
+	push dword [max_open_files]
 	push dword callback
-	push dword [file_path]
+	push dword [source_file_path]
 	call ftw
 	add esp, 12
+	
+	; check for any errors in the ftw() call
+	; error code is returned in EAX register
 	cmp eax, -1
 	je .error
 
+	; yay! no errors.  let the user know and then exit.
 	push printFTWComplete
 	call printf
 	add esp,4
-	
 	jmp .exit
 
 .error:
+	; errors were present.  warn the user and then exit.
 	push printFTWError
 	call printf
 	add esp,4
@@ -70,7 +80,8 @@ TreeWalker:
 callback:
 	push ebp
 	mov ebp,esp
-
+	
+	; display file path and typeflag
 	push dword [ebp+16]
 	push dword [ebp+8]
 	push print_file_found
